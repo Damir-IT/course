61763b90b4ddbdb67b05bfac9508b49d39543536
Artur Davidov <79329347+DawiDov@users.noreply.github.com>
2021-12-08 21:31:10 +0300

    User role based permissions (#4)
    * @login_required + login page NB! needs styling
    
    * user is redirected to "base" ifrom "" if they are already logged in
    
    * Прописать Tab последовательность для логина #2
    
    Co-authored-by: Damir Akhmadullin <damir@Damirs-MacBook-Air.local>

--- course/masterapp/views.py
+++ course/masterapp/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from masterapp.models import  Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required #декоратор проверяющий залогинен ли пользователь 
 
+
+@login_required(login_url='auth_page')
 def base(request):
     menu = Category.objects.all()
     return render(request, 'masterapp/base.html', {'menu': menu, 'title': 'информация к изучению...'})
 
 def auth_page(request):
-    return render(request, 'masterapp/auth_page.html')
+    if request.user.is_authenticated:
+        return redirect('base')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('user')
+            password = request.POST.get('pass')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('base')
+            else:
+                messages.info(request, 'Нам не известен такой человек')
+    
+        return render(request, 'masterapp/auth_page.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect(auth_page)
 
 def pageNotFound(request, exception):
-    return render(request, 'masterapp/page_not_found.html')
+    return render(request, 'masterapp/page_not_found.html') 
