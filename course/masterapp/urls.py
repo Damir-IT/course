f0ece31f4751a7d099f75033e965e8dd25451415
Damir Akhmadullin <damir@Damirs-MacBook-Air.local>
2021-12-08 15:13:34 +0300

    @login_required + login page NB! needs styling

--- course/masterapp/urls.py
+++ course/masterapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import auth_page, base
+from .views import auth_page, base, logout, logout_user
 
 urlpatterns = [
     path('', auth_page, name='auth_page'),
+    path('logout', logout_user, name='logout'),
+    
     path('course/', base, name='base'),
+    
 ]
